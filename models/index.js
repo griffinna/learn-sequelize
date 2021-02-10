1f433488e1b60ce013478368e36a41d613d6a2bf
Griffin <garam.dev9@gmail.com>
2021-02-10 22:04:02 +0900

    Modify: sequelize 객체 생성

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
 db.sequelize = sequelize;
 
 module.exports = db;
